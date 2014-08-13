33db8b663f8e069f721c946c2b70b44c9ca34ef6
Alexis Gallagher <alexis@alexisgallagher.com>
2014-08-13 16:11:53 -0700

    Initial Commit

---
+++ SelfSizingCellsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfSizingCellsDemo
+//
+//  Created by Alexis Gallagher on 2014-08-13.
+//  Copyright (c) 2014 AlexisGallagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
